ef5ecc089cd38a04b2ca7d037b25ed39a31cb22e
ndom91 <yo@ndo.dev>
2025-03-12 13:14:10 +0100

    fix: page title prefix with 'OrcaSlicer'

--- app/docs/[[...slug]]/page.tsx
+++ app/docs/[[...slug]]/page.tsx
@@ -64,7 +64,7 @@ export async function generateMetadata(props: {
   if (!page) notFound();
 
   return {
-    title: page.data.title,
+    title: `OrcaSlicer - ${page.data.title}`,
     description: page.data.description,
   };
 }
